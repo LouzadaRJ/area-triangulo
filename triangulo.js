d50670a2c7014c6344544fa2255adefeb814ec30
LouzadaRJ <matheuslouzadaa@gmail.com>
2023-10-29 22:28:14 -0300

    more details

--- triangulo.js
+++ triangulo.js
@@ -5,6 +5,9 @@ console.log("Área do triangulo: \n");
 let base = entradaDados.question("Informe a base: ");
 let altura = entradaDados.question("Informe a altura: ");
 
+console.log("\nVocê digitou a base: "+base)
+console.log("Você digitou a altura: "+altura)
+
 let area = (base * altura) / 2;
 
-console.log("A área do triangulo é: "+area);
+console.log("\nA área do triangulo é: "+area);
